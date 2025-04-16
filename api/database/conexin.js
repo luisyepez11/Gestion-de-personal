353db11b09f6dcc9a504ab974cc839454e92884d
Valen <valentinamartinez2809gmail.com>
2025-04-16 11:20:06 -0400

    metodo caracter-numero

--- api/database/conexin.js
+++ api/database/conexin.js
@@ -4,7 +4,7 @@ const db = mysql.createConnection(
     {
         host:'localhost',
         user:'root',
-        password:'',
+        password:'Perla15052015*',
         database:'hosp'
     }
 );
